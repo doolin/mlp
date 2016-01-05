90edeea01346c030ec0a3f0ef34b1b4d645b10e2
Dave Doolin <david.doolin@gmail.com>
2016-01-05 04:35:33 -0800

    count words in training set

--- nb/ds/lib/nb.py
+++ nb/ds/lib/nb.py
@@ -1,8 +1,19 @@
 '''a slightly different implementation'''
 
 import re
+from collections import defaultdict
 
 def tokenize(message): #  pylint: disable=missing-docstring
     message = message.lower()
     all_words = re.findall("[a-z0-9']+", message)
     return set(all_words)
+
+def count_words(training_set):
+    """training set consists of pairs (message, is_spam)"""
+    counts = defaultdict(lambda: [0, 0])
+    for message, is_spam in training_set:
+        for word in tokenize(message):
+            counts[word][0 if is_spam else 1] += 1
+    return counts
+
+
--- nb/ds/test/test_nb.py
+++ nb/ds/test/test_nb.py
@@ -8,17 +8,21 @@ import sys
 # from numpy import *
 
 sys.path.append('../lib')
-import nb
 
+import nb #  pylint: disable=import-error
 class TestNB(unittest.TestCase):
 
     def test_tokenize(self):
         message = "foo bar"
         expected = set(['foo', 'bar'])
         actual = nb.tokenize(message)
-        print actual
         self.assertEqual(actual, expected)
 
+    def test_count_words(self):
+        training_set = [("foo bar", 1)]
+        actual = nb.count_words(training_set)
+        expected = {'foo': [1, 0], 'bar': [1, 0]}
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
