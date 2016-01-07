b814b157333a5a1ad6c047f9563b7be265da2009
Dave Doolin <david.doolin@gmail.com>
2016-01-07 04:00:35 -0800

    compute probabilities

--- nb/ds/lib/nb.py
+++ nb/ds/lib/nb.py
@@ -1,9 +1,10 @@
-'''a slightly different implementation'''
+#  pylint: disable=missing-docstring
 
 import re
+import math
 from collections import defaultdict
 
-def tokenize(message): #  pylint: disable=missing-docstring
+def tokenize(message):
     message = message.lower()
     all_words = re.findall("[a-z0-9']+", message)
     return set(all_words)
@@ -23,3 +24,28 @@ def word_probabilities(counts, total_spams, total_non_spams, k=0.5):
              (spam + k) / (total_spams + 2 * k),
              (non_spam + k) / (total_non_spams + 2 * k))
             for w, (spam, non_spam) in counts.iteritems()]
+
+def spam_probabilities(word_probs, message):
+    message_words = tokenize(message)
+    log_prob_if_spam = log_prob_if_not_spam = 0.0
+
+    # iterate through each word in out vocabulary
+    for word, prob_if_spam, prob_if_not_spam in word_probs:
+
+        # if *word* appears in the message,
+        # add the log probability of seeing it
+        if word in message_words:
+            log_prob_if_spam += math.log(prob_if_spam)
+            log_prob_if_not_spam += math.log(prob_if_not_spam)
+
+        # if *word* doesn't appear in the message
+        # add the log probability of _not_ seeing it
+        # which is log(1 - probability of seeing it)
+        else:
+            log_prob_if_spam += math.log(1.0 - prob_if_spam)
+            log_prob_if_not_spam += math.log(1.0 - prob_if_not_spam)
+
+    prob_if_spam = math.exp(log_prob_if_spam)
+    prob_if_not_spam = math.exp(log_prob_if_not_spam)
+    return prob_if_spam / (prob_if_spam + prob_if_not_spam)
+
