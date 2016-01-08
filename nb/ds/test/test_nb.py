676f7231cb1de320a12df04384d26835278ef86f
Dave Doolin <david.doolin@gmail.com>
2016-01-08 05:33:20 -0800

    test word probability function

--- nb/ds/test/test_nb.py
+++ nb/ds/test/test_nb.py
@@ -24,5 +24,20 @@ class TestNB(unittest.TestCase):
         expected = {'foo': [1, 0], 'bar': [1, 0]}
         self.assertEqual(actual, expected)
 
+    def test_word_probabilities(self):
+        training_set = [
+            ("foo bar", 1),
+            ("bar baz", 0)
+        ]
+        counts = nb.count_words(training_set)
+        actual = nb.word_probabilities(counts, 1, 1)
+        expected = [
+            ('baz', 0.25, 0.75),
+            ('foo', 0.75, 0.25),
+            ('bar', 0.75, 0.75)
+        ]
+        self.assertEqual(actual, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
