ef255c4f3039b812cfddbd9df388a1890b176777
Dave Doolin <david.doolin@gmail.com>
2016-01-22 06:18:40 -0800

    step function

--- nb/dss/nn/ruby/lib/perceptron.rb
+++ nb/dss/nn/ruby/lib/perceptron.rb
@@ -1,2 +1,5 @@
 class Perceptron
+  def step_function(x)
+    x > 1 ? 1 : 0
+  end
 end
--- nb/dss/nn/ruby/spec/perceptron_spec.rb
+++ nb/dss/nn/ruby/spec/perceptron_spec.rb
@@ -1,8 +1,18 @@
 require_relative '../lib/perceptron'
 
 describe Perceptron do
+  subject { Perceptron.new }
+
   it "instantiates" do
     expect(Perceptron.new).not_to be_nil
+  end
+
+  it "fires the step function for x > 1" do
+    expect(subject.step_function(3)).to be 1
+  end
+
+  it "fires the step function for x < 1" do
+    expect(subject.step_function(0.2)).to be 0
   end
 end
 
