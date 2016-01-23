113ea97584192e35e4d8e5f8c6b5844bb0480a95
Dave Doolin <david.doolin@gmail.com>
2016-01-23 06:36:16 -0800

    perception implemented in ruby and working

--- nb/dss/nn/ruby/lib/perceptron.rb
+++ nb/dss/nn/ruby/lib/perceptron.rb
@@ -1,5 +1,16 @@
+require 'nmatrix'
+
 class Perceptron
+  def initialize(weights:, bias:)
+    @weights = weights
+    @bias = bias
+  end
+
   def step_function(x)
     x > 1 ? 1 : 0
+  end
+
+  def think(u)
+    step_function(@weights.dot(u).to_f + @bias)
   end
 end
--- nb/dss/nn/ruby/spec/perceptron_spec.rb
+++ nb/dss/nn/ruby/spec/perceptron_spec.rb
@@ -1,10 +1,12 @@
 require_relative '../lib/perceptron'
 
 describe Perceptron do
-  subject { Perceptron.new }
+  let(:weights) { NMatrix.new([4], [1,2,3,4]) }
+  let(:bias) { 0 }
+  subject { Perceptron.new(weights: weights, bias: bias) }
 
   it "instantiates" do
-    expect(Perceptron.new).not_to be_nil
+    expect(subject).not_to be_nil
   end
 
   it "fires the step function for x > 1" do
@@ -13,6 +15,16 @@ describe Perceptron do
 
   it "fires the step function for x < 1" do
     expect(subject.step_function(0.2)).to be 0
+  end
+
+  it "'thinks' when given a new vector" do
+    u  = NMatrix.new([4], [1,2,3,4])
+    expect(subject.think(u)).to be 1
+  end
+
+  it "'thinks' when given a new vector" do
+    u  =NMatrix.new([4], [0,0,0,0])
+    expect(subject.think(u)).to be 0
   end
 end
 
