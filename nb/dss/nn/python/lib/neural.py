b295e9f5a8fc4847c470a2702342a7d8d696a7a1
Dave Doolin <david.doolin@gmail.com>
2016-01-31 06:48:06 -0800

    perceptron in python

--- nb/dss/nn/python/lib/neural.py
+++ nb/dss/nn/python/lib/neural.py
@@ -1,3 +1,11 @@
 # pylint: disable=missing-docstring
+
+import numpy
+
 def step_function(x): #  pylint: disable=invalid-name
     return 1 if x >= 0 else 0
+
+def perceptron_output(weights, bias, x):
+    """returns 1 if the perceptron 'fires', 0 if not"""
+    calculation = numpy.dot(weights, x) + bias
+    return step_function(calculation)
--- nb/dss/nn/python/test/test_neural.py
+++ nb/dss/nn/python/test/test_neural.py
@@ -18,5 +18,14 @@ class TestNB(unittest.TestCase):
         actual = neural.step_function(1)
         self.assertEqual(actual, expected)
 
+    def test_perceptron_output(self):
+        weights = [1, 1]
+        bias = 1.0
+        x = [1, 2]
+        expected = 1.0
+        actual = neural.perceptron_output(weights, bias, x)
+        self.assertEqual(actual, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
