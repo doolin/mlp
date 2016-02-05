f12d5e04d6392c09f39b077fc2edbe9c7ab71e66
Dave Doolin <david.doolin@gmail.com>
2016-02-05 06:15:43 -0800

    sigmoid only today, busy

--- nb/dss/nn/python/lib/neural.py
+++ nb/dss/nn/python/lib/neural.py
@@ -1,11 +1,15 @@
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,invalid-name
 
+import math
 import numpy
 
-def step_function(x): #  pylint: disable=invalid-name
+def step_function(x):
     return 1 if x >= 0 else 0
 
 def perceptron_output(weights, bias, x):
     """returns 1 if the perceptron 'fires', 0 if not"""
     calculation = numpy.dot(weights, x) + bias
     return step_function(calculation)
+
+def sigmoid(t):
+    return 1 / (1 + math.exp(-t))
