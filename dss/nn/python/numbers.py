f7ae3ac2f396fe2a624314719e7a9644ef849fc6
Dave Doolin <david.doolin@gmail.com>
2016-02-20 06:45:57 -0800

    started into processing code

--- dss/nn/python/numbers.py
+++ dss/nn/python/numbers.py
@@ -71,5 +71,20 @@ eight_digit = [
     1,0,0,0,1,
     1,1,1,1,1
 ]
+nine_digit = [
+    1,1,1,1,1,
+    1,0,0,0,1,
+    1,1,1,1,1,
+    0,0,0,0,1,
+    1,1,1,1,1
+]
+
+targets = [[1 if i == j else 0 for i in range(10)]
+           for j in range(10)]
+
+random.seed(0)   # to get repeatable results
+input_size = 25  # each input is a vector of length 25
+num_hidden = 5   # we'll have 5 neurons in the hidden layer
+output_size = 10 # we need 10 outputs for each input
 
 
