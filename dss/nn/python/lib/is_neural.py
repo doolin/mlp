4a744ca3c8d977469cf0a4c66ae36dae2ac3c9d6
Dave Doolin <david.doolin@gmail.com>
2016-02-24 06:52:46 -0800

    progress in inches is still progress

--- dss/nn/python/lib/is_neural.py
+++ dss/nn/python/lib/is_neural.py
@@ -3,62 +3,61 @@
 import math
 import numpy
 
-class IsNeural(object):
-    def step_function(x):
-        return 1 if x >= 0 else 0
+def step_function(x):
+    return 1 if x >= 0 else 0
 
-    def perceptron_output(weights, bias, x):
-        """returns 1 if the perceptron 'fires', 0 if not"""
-        calculation = numpy.dot(weights, x) + bias
-        return step_function(calculation)
+def perceptron_output(weights, bias, x):
+    """returns 1 if the perceptron 'fires', 0 if not"""
+    calculation = numpy.dot(weights, x) + bias
+    return step_function(calculation)
 
-    def sigmoid(t):
-        return 1 / (1 + math.exp(-t))
+def sigmoid(t):
+    return 1 / (1 + math.exp(-t))
 
-    def neuron_output(weights, inputs):
-        return sigmoid(numpy.dot(weights, inputs))
+def neuron_output(weights, inputs):
+    return sigmoid(numpy.dot(weights, inputs))
 
-    def feed_forward(neural_network, input_vector):
-        """takes in a neural network
-        (represented as a list of lists of lists of weights)
-        and returns the output from forward-propagating the input"""
+def feed_forward(neural_network, input_vector):
+    """takes in a neural network
+    (represented as a list of lists of lists of weights)
+    and returns the output from forward-propagating the input"""
 
-        outputs = []
+    outputs = []
 
-        # process one layer at a time
-        for layer in neural_network:
-            input_with_bias = input_vector + [1]
-            output = [neuron_output(neuron, input_with_bias)
-                      for neuron in layer]
-            outputs.append(output)
+    # process one layer at a time
+    for layer in neural_network:
+        input_with_bias = input_vector + [1]
+        output = [neuron_output(neuron, input_with_bias)
+                  for neuron in layer]
+        outputs.append(output)
 
-            # then the input to the next layer is the output of this one
-            input_vector = output
+        # then the input to the next layer is the output of this one
+        input_vector = output
 
-        return outputs
+    return outputs
 
-    #def backpropagate(network, input_vector, targets):
-    def foobar(network, input_vector, targets):
+#def backpropagate(network, input_vector, targets):
+def backpropagate(network, input_vector, targets):
 
-        hidden_outputs, outputs = feed_forward(network, input_vector)
+    hidden_outputs, outputs = feed_forward(network, input_vector)
 
-        # the output * (1 - output) is from the derivative of sigmoid
-        output_deltas = [output * (1 - output) * (output - target)
-                         for output, target in zip(outputs, targets)]
+    # the output * (1 - output) is from the derivative of sigmoid
+    output_deltas = [output * (1 - output) * (output - target)
+                     for output, target in zip(outputs, targets)]
 
-        # adjust weights for output layer, one neuron at a time
-        for i, output_neuron in enumerate(network[-1]):
-            # focus on the ith output layer neuron
-            for j, hidden_output in enumerate(hidden_outputs + [1]):
-                # adjust the jth weight based on both
-                # this neuron's delta and its jth input
-                output_neuron[j] -= output_deltas[i] * hidden_output
+    # adjust weights for output layer, one neuron at a time
+    for i, output_neuron in enumerate(network[-1]):
+        # focus on the ith output layer neuron
+        for j, hidden_output in enumerate(hidden_outputs + [1]):
+            # adjust the jth weight based on both
+            # this neuron's delta and its jth input
+            output_neuron[j] -= output_deltas[i] * hidden_output
 
-        hidden_deltas = [hidden_output * (1 - hidden_output) *
-                         dot(output_deltas, [n[i] for n in output_layer])
-                         for i, hidden_output in enumerate(hidden_outputs)]
+    hidden_deltas = [hidden_output * (1 - hidden_output) *
+                     dot(output_deltas, [n[i] for n in output_layer])
+                     for i, hidden_output in enumerate(hidden_outputs)]
 
-        # adjust weights for hidden layer, one neuron at a time
-        for i, hidden_neuron in enumerate(network[0]):
-            for j, input in enumerate(input_vector + [1]):
-                hidden_neuron[j] -= hidden_deltas[i] * input
+    # adjust weights for hidden layer, one neuron at a time
+    for i, hidden_neuron in enumerate(network[0]):
+        for j, input in enumerate(input_vector + [1]):
+            hidden_neuron[j] -= hidden_deltas[i] * input
